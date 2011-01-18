29ba0e84a263b9d9d644e42a107ca0d3824b5882
Kazuho Oku <kazuhooku@gmail.com>
2011-01-18 14:59:32 +0900

    releng for 0.11

--- lib/Server/Starter.pm
+++ lib/Server/Starter.pm
@@ -15,7 +15,7 @@ use Scope::Guard;
 
 use Exporter qw(import);
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 our @EXPORT_OK = qw(start_server restart_server server_ports);
 
 my @signals_received;
