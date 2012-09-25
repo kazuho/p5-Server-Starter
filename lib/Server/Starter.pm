0ef253ca39a7abe1ba5b5bd83267bccd7bbab672
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-09-25 13:13:28 +0900

    Added --signal-on-term
    This allows sending a signal (other than TERM) when start_server
    receives TERM. Previously it always sends TERM to the server, which
    might shuts down the process immediately without waiting for it to
    shutdown gracefully.

--- lib/Server/Starter.pm
+++ lib/Server/Starter.pm
@@ -26,11 +26,14 @@ sub start_server {
     };
     $opts->{interval} = 1
         if not defined $opts->{interval};
-    $opts->{signal_on_hup} ||= 'TERM';
-    # normalize to the one that can be passed to kill
-    $opts->{signal_on_hup} =~ tr/a-z/A-Z/;
-    $opts->{signal_on_hup} =~ s/^SIG//i;
-    
+    $opts->{signal_on_hup}  ||= 'TERM';
+    $opts->{signal_on_term} ||= 'TERM';
+    for ($opts->{signal_on_hup}, $opts->{signal_on_term}) {
+        # normalize to the one that can be passed to kill
+        tr/a-z/A-Z/;
+        s/^SIG//i;
+    }
+
     # prepare args
     my $ports = $opts->{port};
     my $paths = $opts->{path};
@@ -162,6 +165,7 @@ sub start_server {
         };
     
     # the main loop
+    my $term_signal;
     $current_worker = _start_worker($opts);
     $update_status->();
     while (1) {
@@ -192,6 +196,7 @@ sub start_server {
                 kill $opts->{signal_on_hup}, $_
                     for sort keys %old_workers;
             } else {
+                $term_signal = $signals_received[0] eq 'TERM' ? $opts->{signal_on_term} : 'TERM';
                 goto CLEANUP;
             }
         }
@@ -201,9 +206,10 @@ sub start_server {
     # cleanup
     $old_workers{$current_worker} = $ENV{SERVER_STARTER_GENERATION};
     undef $current_worker;
-    print STDERR "received $signals_received[0], sending TERM to all workers:",
+
+    print STDERR "received $signals_received[0], sending $term_signal to all workers:",
         join(',', sort keys %old_workers), "\n";
-    kill 'TERM', $_
+    kill $term_signal, $_
         for sort keys %old_workers;
     while (%old_workers) {
         if (my @r = wait3(1)) {
