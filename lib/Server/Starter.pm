96d350fa75baee8521ab14a58e7b196175b039d5
Kazuho Oku <kazuhooku@gmail.com>
2015-04-01 15:11:24 +0900

    releng for 0.24

--- lib/Server/Starter.pm
+++ lib/Server/Starter.pm
@@ -13,7 +13,7 @@ use Server::Starter::Guard;
 
 use Exporter qw(import);
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 our @EXPORT_OK = qw(start_server restart_server server_ports);
 
 my @signals_received;
